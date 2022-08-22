bd3cb52951af5dc040d99bb9b1de49dd65ec4d3f
Artur Faria - T8 <artur.f.vieira.94@gmail.com>
2022-08-21 23:24:23 -0300

    viracartas,verificaigual,desvira

--- começajogo.js
+++ começajogo.js
@@ -25,15 +25,58 @@ function começajogo(){
     console.log(cartasjogo);
     let divcartas = document.querySelector(".CaixaCartas");
 
-    console.log(divcartas);
-
-    for(i=0; i<cartasjogo.length; i++){
+        for(i=0; i<cartasjogo.length; i++){
         let txt = cartasjogo[i]
-        divcartas.innerHTML += `<div class = "${txt} Cartas" onclick="virarcarta()">
-        <img src ="imgs/front.png">
-                </div>`;
+        divcartas.innerHTML += `<div class = "${txt} Cartas">
+        <div class="front-face face">
+             <img src ="imgs/${txt}.gif">               
+        </div>
+        <div class="back-face face" onclick="virarcarta(this)" >
+            <img src ="imgs/front.png">
+        </div>`
 
 
     }
 }
+let primeiracarta = '';
+let segundacarta = '';
+function virarcarta(cartaclicada){
+    
+    if(primeiracarta !== '' && segundacarta !==''){
+        console.log(primeiracarta);
+        console.log(segundacarta);
+        
+        verificaigual(primeiracarta, segundacarta);
+    }
+    
+    if(primeiracarta=== ''){
+        primeiracarta = cartaclicada;
+        primeiracarta.classList.add('virada');
+    }
+    else {segundacarta = cartaclicada;
+        segundacarta.classList.add('virada');
+        verificaigual(primeiracarta, segundacarta);
+    }
 
+   
+}
+ function verificaigual(c1, c2){  
+      
+    if(c1.parentNode.classList[0]===c2.parentNode.classList[0]){
+    primeiracarta= '';
+    segundacarta= '';     
+}
+    else {
+    
+    setTimeout(desvira, 1000,c1, c2);
+}
+   
+}
+function desvira(a, b){
+    let desvira1 =a;
+    let desvira2 =b;
+    desvira1.classList.remove('virada');
+    desvira2.classList.remove('virada');
+    primeiracarta= '';
+    segundacarta= '';
+}
